23b4419b1b79906765e7c9512c0230a0cadd2d8f
Yared Terefe <yaredterefeg@gmail.com>
2022-12-23 12:52:46 +0300

    fixes

---
+++ lib/esm/types/components/PDFViewer/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import "./index.css";
+type PDFViewrProps = {
+    PDF: string;
+};
+declare function PDFViewer({ PDF }: PDFViewrProps): JSX.Element;
+export default PDFViewer;
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/esm/types/index.d.ts
@@ -0,0 +1,3 @@
+import PDFViewer from "./components/PDFViewer/index.jsx";
+export default PDFViewer;
+//# sourceMappingURL=index.d.ts.map
